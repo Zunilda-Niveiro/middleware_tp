c3a940f17b8ecc63d9df1a62ee7f0332374caac7
Zunilda-Niveiro <niveiro.zunilda@gmail.com>
2022-08-29 14:46:25 -0300

    subiendo tp

---
+++ src/routes/main.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const {index, contact, services, about, admin, login} = require('../controllers/mainController');
+
+const adminCheck = require('../middlewares/adminCheck');
+router
+    .get('/', index)
+    .get('/contact', contact)
+    .get('/about', about)
+    .get('/services', services)
+    .get('/admin',adminCheck, admin)
+    .get('/login', login)
+
+
+module.export = router;
